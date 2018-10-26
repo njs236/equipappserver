6ace97242a65025a50da5b857d839a4515b277c9
Nathan Sinclair <nathansinclair@Nathans-Mini.fritz.box>
2018-10-27 09:53:40 +1300

    first commit

---
+++ api/noti.php
@@ -0,0 +1,21 @@
+<?php
+	
+// sends notification to device. use FCM.
+
+// get user_id of the person sending.
+
+// for each user that is using app.
+//requires subscription id. store in db
+// construct notification.
+// title
+// message
+// apple notification contents
+// send using FCM sender class
+
+	
+	
+	
+	
+	
+	
+?>
